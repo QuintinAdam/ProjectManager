40f2c59150ebe5746b25e90cbe927a3db9121893
Quintin Adam <QuintinAdam@live.com>
2013-11-09 12:49:09 -0700

    adds the tasks model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :goals
-  attr_accessible :description, :name, :notes, :goal_attributes
+  has_many :tasks
+  attr_accessible :description, :name, :notes, :goal_attributes, :task_attributes
   accepts_nested_attributes_for :goals, allow_destroy: true
+  accepts_nested_attributes_for :tasks, allow_destroy: true
 end
