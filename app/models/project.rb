8ea62b7d618325be4e2697af92b7fbdbda6d278d
ferg <joey@fergmedia.com>
2013-11-09 16:01:26 -0700

    Fixes 'conversation' issue on projects controller. Styles index page.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   has_many :goals
   has_many :tasks
   has_many :members
-  attr_accessible :description, :name, :notes, :goal_attributes, :task_attributes, :member_attributes
+  attr_accessible :description, :name, :notes, :goals_attributes, :tasks_attributes, :members_attributes
   accepts_nested_attributes_for :goals, allow_destroy: true
   accepts_nested_attributes_for :tasks, allow_destroy: true
   accepts_nested_attributes_for :members, allow_destroy: true
