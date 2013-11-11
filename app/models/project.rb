87cd05b9012571b499f9471f373c5f261fc0f3e3
Quintin Adam <QuintinAdam@live.com>
2013-11-11 10:18:36 -0700

    improvments to the new projects form.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
   has_many :tasks
   has_many :members
   attr_accessible :description, :name, :notes, :goals_attributes, :tasks_attributes, :members_attributes
-  accepts_nested_attributes_for :goals, allow_destroy: true
-  accepts_nested_attributes_for :tasks, allow_destroy: true
-  accepts_nested_attributes_for :members, allow_destroy: true
+  accepts_nested_attributes_for :goals, reject_if: proc { |attributes| attributes['goal'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :tasks, reject_if: proc { |attributes| attributes['task'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :members, :reject_if => :all_blank, allow_destroy: true
 end
